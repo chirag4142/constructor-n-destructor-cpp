c4e9feff34565e7955f9675a1f79a0751589a74f
chirag tekwani <ctekwani051@gmail.com>
2024-10-16 13:26:20 +0530

    Create default_constructor.cpp

---
+++ default_constructor.cpp
@@ -0,0 +1,26 @@
+// Name : chirag tekwani
+// PRN : 23070123042
+
+#include <iostream>
+using namespace std;
+class construct{
+    public:
+    int a,b;
+
+construct(){
+    a = 10;
+    b = 20;
+}
+void display(){
+cout << "a ="<< a <<endl<< "b ="<<b;
+}
+};
+
+
+int main() {
+    construct constructor;
+    constructor.display();
+    
+
+    return 0;
+}
